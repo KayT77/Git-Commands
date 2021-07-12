707d7a45836cb8a02224b32c736e3f088f7a532f
Kelvin <81403130+KayT77@users.noreply.github.com>
2021-07-11 23:46:35 -0600

    Add files via upload

---
+++ try.py
@@ -0,0 +1,30 @@
+
+
+
+countries = ["Uk","Gh", 77 , "Ng", False, "AU","NZ"]
+#countries[0] = "USA"
+#countries [5] = "Canada"
+
+#print(countries[-2])                          count from behind, find specific index ,length of string 
+#print(countries)                               from point to point ,from specified point to end, type clase i.e str,int,bool ?//
+print(type(countries[4]))                        
+#print(countries[3:])
+#print(countries[0:4])
+#print(len(countries))
+#print(countries.index("NZ"))
+
+
+#age = 77
+#print(age)
+#name = "Kelvin" 
+#print(type(name))
+
+
+countries2 = list(("ghana", "nigeria", 77, False ))
+
+print(countries2)
+print(type(countries2))
+
+
+              #List Methods 
+
